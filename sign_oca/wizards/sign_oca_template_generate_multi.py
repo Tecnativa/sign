1e3fe5dfaff74c0bfdb199d353b0556be9e98aae
Víctor Martínez <victor.martinez@tecnativa.com>
2024-03-05 09:37:54 +0100

    [IMP] sign_oca: Add Sign from template action from tree and form view
    TT41745

--- sign_oca/wizards/sign_oca_template_generate_multi.py
+++ sign_oca/wizards/sign_oca_template_generate_multi.py
@@ -1,34 +1,20 @@
-# Copyright 2023 Tecnativa - Víctor Martínez
+# Copyright 2023-2024 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class SignOcaTemplateGenerateMulti(models.TransientModel):
     _name = "sign.oca.template.generate.multi"
     _description = "Generate signature requests"
 
-    model = fields.Char(string="Model", readonly=True)
-    model_id = fields.Many2one(
-        comodel_name="ir.model",
-        compute="_compute_model_id",
-        required=True,
-    )
+    model = fields.Char(readonly=True)
     template_id = fields.Many2one(
         comodel_name="sign.oca.template",
-        domain="['|', ('model_id', '=', False),('model_id', '=', model_id)]",
+        domain="['|', ('model', '=', False),('model', '=', model)]",
         required=True,
     )
     message = fields.Html()
-
-    @api.depends("model")
-    def _compute_model_id(self):
-        ir_model = self.env["ir.model"]
-        for item in self:
-            if item.model:
-                item.model_id = ir_model.search([("model", "=", item.model)])
-            else:
-                item.model_id = item.model_id
 
     def _prepare_sign_oca_request_vals(self):
         vals = []
